c84a016c7765510b994cc3cb99ad320f6292d022
AarslandM <aarsland.martin@gmail.com>
2022-09-24 20:30:55 +0200

    Added more fishes

--- FishSpawner.gd
+++ FishSpawner.gd
@@ -1,6 +1,14 @@
 extends Node
 
-onready var fishes : Array = [preload("res://Fish.tscn")]
+onready var fishes : Array = [
+	preload("res://fish/Fish.tscn"), 
+	preload("res://fish/Fish2.tscn"),
+	preload("res://fish/Fish3.tscn"),
+	preload("res://fish/Fish4.tscn"),
+	preload("res://fish/Fish5.tscn"),
+	preload("res://fish/Fish6.tscn"),
+	preload("res://fish/Fish7.tscn"),
+	preload("res://fish/Fish8.tscn"),]
 
 func _spawn_fish():
 	var index = randi() % fishes.size()
